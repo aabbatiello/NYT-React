c79a0440dfe862a01124bf15d3c521727b60384e
aabbatiello <anthonyjabbatiello@gmail.com>
2017-08-23 20:34:02 -0400

    updated files

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-'use strict'
-
 // Include Server Dependencies
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+mongoose.Promise = Promise;
 
-// Require Article Schema
-const Article = require("./models/Article");
-
-// Create Instance of Express
-const app = express();
-// Sets an initial port.
-const PORT = process.env.PORT || 3000;
+//Requiring my schema here. 
+var Article = require("./models/Article");
+//Create an instance of express
+var app = express();
+//Sets an initial port. We'll use this later in our listener.
+var PORT = process.env.PORT || 3000;
 
 // Run Morgan for Logging
 app.use(logger("dev"));
@@ -21,78 +20,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static("./public"));
+//Setting a static path.
+app.use(express.static(path.join((__dirname, "./public"))));
 
-// -------------------------------------------------
+//Requiring the routes from the controllers.js file
+require("./controllers/controllers.js")(app);
 
-// MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("mongodb:/localhost/nytimes");
+//Connecting to the heroku mongo db database. 
+mongoose.connect("/localhost/ny_scrape");
+
+//Creating a connection variable for mongoose for easier writing. 
 var db = mongoose.connection;
 
-db.on("error", function(err) {
-  console.log("Mongoose Error: ", err);
+//Setting up console logs for mongoose errors. 
+db.on("error", function(err){
+    console.log("Mongoose Error: ", err);
 });
-
+// Notifying me once the connection is successful via console log. 
 db.once("open", function() {
-  console.log("Mongoose connection successful.");
+    console.log("Mongoose connection successful!");
 });
 
-// -------------------------------------------------
-
-// Route to get all saved articles.
-app.get("/api/saved", function(req, res) {
-
-  // We will find all the records, sort it in descending order, then limit the records to 5
-  Article.find({}).limit(10).exec(function(err, doc) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      res.send(doc);
-    }
-  });
-});
-
-// Main "/" Route. Redirects user to rendered React application.
-app.get("*", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-// Route to save articles from searches.
-app.post("/api/saved", function(req, res) {
-  console.log("Article title: " + req.body.title);
-  console.log("Article date: " + req.body.date);
-  console.log("Article url: ") + req.body.url;
-
-  // Save article.
-  Article.create({
-    title: req.body.title,
-    date: req.body.date,
-    url: req.body.url
-  }, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      res.send("Saved Article");
-    }
-  });
-});
-
-// Route to delete saved article.
-app.delete("/api/saved/:id", function(req, res) {
-
-  console.log("Article ID to delete: " + req.params.id);
-
-  Article.findByIdAndRemove(req.params.id, function (err, response) {
-    if(err){
-      res.send("Delete didn't work: " + err);
-    }
-    res.send(response);
-  });
-});
-
-// Listener.
-app.listen(PORT, () => {
+//Setting up a successful port listener console log. 
+app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
